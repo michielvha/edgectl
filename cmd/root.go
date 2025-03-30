898fb80d8b183bf1732cf3131ed58eb5fd0713de
DL6544 <DL6544@engie.com>
2025-03-30 22:11:14 +0200

    updating rootcmd

--- cmd/root.go
+++ cmd/root.go
@@ -12,16 +12,12 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "edgectl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A CLI for Edge Cloud",
+	Long: `This CLI is a tool for managing edge cloud resources.
+It provides commands for creating, updating, and deleting resources
+in an edge cloud environment.
+It is designed to be easy to use and efficient, allowing users
+to quickly manage their edge cloud resources from the command line.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
